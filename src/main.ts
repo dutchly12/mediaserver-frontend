313994a1a28d01554421d6473cc7a7d99f47f7d2
Nikita Ignatovich <nikita.i@dashbouquet.com>
2025-02-13 00:49:38 +0300

    rename app.vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import App from '@/app.vue'
 
 import pinia from '@/configurations/pinia'
 import router from '@/configurations/router'
