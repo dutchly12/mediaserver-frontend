4c1254d2742f61bed39956a9eb5c081cefcb2a0d
Nikita Ignatovich <nikita.i@dashbouquet.com>
2025-03-16 23:19:10 +0300

    optimize classes

--- src/components/layouts/default.vue
+++ src/components/layouts/default.vue
@@ -6,7 +6,7 @@ import AppMenu from '@/components/app/menu.vue';
   <div>
     <AppMenu />
 
-    <div class="mt-14.5 md:ml-62 md:mt-2">
+    <div class="mt-14.5 mx-2" md="ml-62 mt-2">
       <slot />
     </div>
   </div>
