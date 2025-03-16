de6c2073aee557d0b3b45b5dd9a70f9a4938d398
Nikita Ignatovich <nikita.i@dashbouquet.com>
2025-03-16 23:19:59 +0300

    add uploader

---
+++ src/components/page/collections/id/uploader.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useApi } from '@/composables/use-api.ts';
+import { useStorageStore } from '@/stores/storage.ts';
+import UiIcon from '@/components/ui/icon.vue';
+import { useTemplateRef } from 'vue';
+
+const api = useApi();
+const storageStore = useStorageStore();
+
+const input = useTemplateRef('input');
+
+const props = defineProps<{
+  collectionId: string;
+}>();
+
+const handleClick = () => {
+  input.value?.click();
+};
+
+const handleUploadEvent = async (file: string, name: string) => {
+  try {
+    await api.videos.create({ video: { name, collection_id: props.collectionId, file } });
+  } catch {}
+};
+
+const handleChange = (event: Event) => {
+  const { files } = event.target as HTMLInputElement;
+  const filesArray = [...(files ?? [])];
+
+  filesArray.forEach((file) => {
+    storageStore.upload(file, handleUploadEvent);
+  });
+};
+</script>
+
+<template>
+  <div>
+    <UiIcon name="cloud-upload" class="size-12 stroke-1.5 cursor-pointer" @click="handleClick" />
+    <input ref="input" type="file" multiple class="hidden" @change="handleChange" />
+  </div>
+</template>
--- src/pages/collections/[id]/index.vue
+++ src/pages/collections/[id]/index.vue
@@ -4,6 +4,8 @@ import { useRoute } from 'vue-router';
 import { useHead } from '@unhead/vue';
 import { useApi } from '@/composables/use-api.ts';
 import UiText from '@/components/ui/text.vue';
+import PageCollectionIdUploader from '@/components/page/collections/id/uploader.vue';
+import PageCollectionsIdVideo from '@/components/page/collections/id/video.vue';
 import type { Collection } from '@/types/model/collection.ts';
 
 const route = useRoute();
@@ -31,16 +33,20 @@ useHead(() => ({
 </script>
 
 <template>
-  <div>
-    <UiText variant="h2">
-      {{ collection?.name }}
-    </UiText>
+  <div class="flex flex-col gap-4">
+    <div class="p-2 flex justify-between items-center border-b">
+      <UiText variant="h2">
+        {{ collection?.name }}
+      </UiText>
 
     <div>
       <RouterLink v-for="video in collection?.videos" :key="video.id" :to="{ name: 'videos-id', params: { id: video.id} }">
         <img :src="video.preview" :alt="video.name" loading="lazy" />
         <UiText>{{ video.name }}</UiText>
       </RouterLink>
+      <PageCollectionIdUploader :collection-id="routeId" />
+    </div>
+
     </div>
   </div>
 </template>
