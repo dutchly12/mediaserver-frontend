18f1d87200cc0ba8649d1ee72ab590038d4300dc
Nikita Ignatovich <nikita.i@dashbouquet.com>
2025-10-28 21:05:02 +0300

    self-destroy

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      selfDestroying: true,
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Mediaserver',
